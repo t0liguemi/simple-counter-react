cf6d02740e08528d9a3a1c03ce31fbe9531b94fb
unknown <toliguemi94@gmail.com>
2023-11-02 01:29:30 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { AiOutlineClockCircle } from 'react-icons/ai';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="d-grid bg-black py-3 px-4 m-4 fs-1">
+        <div className="row">
+          <div className="col d-flex justify-content-center w-50 m-4">
+            <div className="bg-dark bg-secondary m-2 p-3 text-white rounded border-light"><AiOutlineClockCircle /></div>
+            <div className="bg-dark bg-secondary m-2 p-3 text-white rounded border-black">{props.tenthousands}</div>
+            <div className="bg-dark bg-secondary m-2 p-3 text-white rounded border-black">{props.thousands}</div>
+            <div className="bg-dark bg-secondary m-2 p-3 text-white rounded border-black">{props.hundreds}</div>
+            <div className="bg-dark bg-secondary m-2 p-3 text-white rounded border-black">{props.tenths}</div>
+            <div className="bg-dark bg-secondary m-2 p-3 text-white rounded border-black">{props.units}</div>
+          </div>
+        </div>
+        <div className="row m-4">
+          <form onSubmit={(e)=>props.countdown(e)} className="col d-flex justify-content-center flex-column p-3" id="countdownForm">
+            <input className="form-control my-1" type="text" name="countdownInput"/>
+            <button type="submit" className="btn btn-secondary my-1" >Countdown</button>
+          </form>
+          <div className="col d-flex justify-content-center flex-column p-3">
+            <button onClick={props.pauseTrigger} className="btn btn-secondary my-1">Pause</button>
+            <button onClick={props.resumeTrigger} className="btn btn-secondary my-1">Resume</button>
+            <button onClick={props.resetTrigger} className="btn btn-secondary my-1">Reset</button>
+          </div>
+          <form onSubmit={(e)=>props.alertTrigger(e)} className="col d-flex justify-content-center flex-column p-3">
+            <input className="form-control my-1" type="text" name="alertTarget" />
+            <button type="submit" className="btn btn-secondary my-1">Alert after</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Defaults
+let secondsCount = 0;
+let tenthCount = 0;
+let hundredCount = 0;
+let thousandCount = 0;
+let tenthousandCount = 0;
+let areWeCountingDown = false;
+let areWePaused = false;
+let doWeAlert = false;
+let alertTarget = 0;
+
+function countdown(event) {
+  event.preventDefault();
+  areWeCountingDown = true;
+  doWeAlert = false;
+  let form = event.target;
+  let formData = new FormData(form);
+  const formEntries = Object.fromEntries(formData.entries());
+  console.log(formEntries.countdownInput);
+  secondsCount = formEntries.countdownInput
+}
+function alertTrigger(event){
+  event.preventDefault();
+  areWeCountingDown = false;
+  doWeAlert = true;
+  areWePaused = false;
+  let form = event.target;
+  let formData = new FormData(form);
+  secondsCount=0
+  const formEntries = Object.fromEntries(formData.entries());
+  alertTarget=formEntries.alertTarget;
+  console.log(formEntries.alertTarget);
+}
+function pauseTrigger(){
+  areWePaused = true;
+}
+function resumeTrigger(){
+  areWePaused = false;
+}
+function resetTrigger(){
+  secondsCount=0
+  areWeCountingDown = false;
+  doWeAlert = false;
+}
+setInterval(() => {
+  if (secondsCount<1 && areWeCountingDown){
+    areWePaused = true
+  }
+  if (areWePaused){
+  } else if (areWeCountingDown){
+    secondsCount -= 1
+  }else{
+    secondsCount += 1
+  }
+  if(secondsCount === parseInt(alertTarget) && doWeAlert){
+    alert("Your time is over")
+  }
+  tenthCount = Math.floor(secondsCount / 10);
+  hundredCount = Math.floor(secondsCount / 100);
+  thousandCount = Math.floor(secondsCount / 1000);
+  tenthousandCount = Math.floor(secondsCount / 10000);
+  root.render(
+    <React.StrictMode>
+      <App
+        countdown={countdown}
+        pauseTrigger={pauseTrigger}
+        resumeTrigger={resumeTrigger}
+        resetTrigger={resetTrigger}
+        alertTrigger={alertTrigger}
+        units={secondsCount % 10}
+        tenths={tenthCount % 10}
+        hundreds={hundredCount % 10}
+        thousands={thousandCount % 10}
+        tenthousands={tenthousandCount % 10}
+      />
+    </React.StrictMode>
+  );
+}, 1000)
 reportWebVitals();
